b3a41b0e8cb57ca8d77e1718aba331c51e3afd5a
chris <christian@denker.io>
2020-10-13 16:47:17 +0200

    added ITickExecution doku

--- src/main/java/org/scheduler/agent/behaviour/ITickExecution.java
+++ src/main/java/org/scheduler/agent/behaviour/ITickExecution.java
@@ -1,10 +1,16 @@
 package org.scheduler.agent.behaviour;
 
+import java.time.temporal.TemporalAdjuster;
+
 import org.scheduler.Tick;
 import org.scheduler.agent.Agent;
 
 public interface ITickExecution {
 	
+	/**
+	 * executes a {@link Tick}. The {@link Tick} contains a {@link TemporalAdjuster} that represents the duration/timespan that is to be executed
+	 * @param tick
+	 */
 	public void execute(Tick tick);
 	
 	/**
